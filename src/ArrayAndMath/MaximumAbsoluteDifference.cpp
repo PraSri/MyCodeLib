2cf5462b230f8e03f62f6b520246fe3fede8f4ea
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2020-10-09 00:53:51 +0530

    Update MaximumAbsoluteDifference.cpp

--- src/ArrayAndMath/MaximumAbsoluteDifference.cpp
+++ src/ArrayAndMath/MaximumAbsoluteDifference.cpp
@@ -15,3 +15,12 @@ int Solution::maxArr(vector<int> &A) {
 /*You are given an array of N integers, A1, A2, .... AN.
  *Return the maximum value of f(i, j) for all 1 ≤ i, j ≤ N. f(i, j) is defined as
  *Return  |A[i] - A[j]| + |i - j|, where |x| denotes absolute value of x.*****/
+/**
+	 * 
+	 * |A[i]-A[j]| + |i-j|
+	 * remove modulus according to modulus function:
+	 * (a[i]+i)-(a[j]+j) or (a[j]+j)-(a[i]+i)
+	 * For max value = MAX-MIN
+	 * 
+	 * 
+	 * */
