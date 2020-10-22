78ce8ac688abd62a9cc15d190fdabc4b6987fb4b
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2020-10-22 15:16:08 +0530

    Create ConvertBinartTreeIntoItsMirror.java

---
+++ src/Trees/ConvertBinartTreeIntoItsMirror.java
@@ -0,0 +1,15 @@
+/**
+	 * Algorithm – Mirror(tree):
+	 * 
+	 * (1) Call Mirror for left-subtree i.e., Mirror(left-subtree)
+	 *  (2) Call Mirror for right-subtree i.e., Mirror(right-subtree) 
+	 *  (3) Swap left and right subtrees :
+	 *  temp = left-subtree 
+	 *  left-subtree = right-subtree 
+	 *  right-subtree = temp
+	 * 
+	 * 
+	 * 
+	 */
+   
+   
