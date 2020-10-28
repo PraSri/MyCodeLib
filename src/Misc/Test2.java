5d4d449a0870827a444efe6c64d614a1a9d65ebf
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2020-10-28 18:34:55 +0530

    Add files via upload

---
+++ src/Misc/Test2.java
@@ -0,0 +1,41 @@
+package TestPackage;
+
+public class Test2 {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(fastPower(2, 4, 1000000000)); // output = 16
+	}
+
+	public static int fastPower(long a, long b, long mod) {
+		
+		
+		long res = 1; // a^b
+		
+		
+		while (b > 0) {
+			
+			// b&1 => isolates the last bit and check is it 1?
+			if ((b & 1) == 1) {
+				
+				// if yes then it means that the power number(b) is odd
+				
+				res = res * a % mod;
+				
+				// save the result like this : if a = 2 b =  5 a^b = 2^5 res = res * 2 
+				// res = 1*2 = 2;
+				
+			}
+			
+			// update a : a = 2 * 2=4
+			a = a * a % mod;
+
+			// divide b by 2 : b = b>>1 => b = 2.5 => b=2
+			b >>= 1;
+		}
+		
+		
+		return (int) res;
+	}
+}
