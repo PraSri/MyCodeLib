273c3e2b7ffe0c77241339b3ecb7317761a9e21c
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2020-11-09 17:59:40 +0530

    Update ImplementStrStr.java

--- src/String/ImplementStrStr.java
+++ src/String/ImplementStrStr.java
@@ -5,7 +5,12 @@ public class ImplementStrStr {
 	public static void main(String[] args) {
 
 	}
-
+/*****
+	 * locate a substring ( needle ) in a string ( haystack ). Try not to use
+	 * standard library string functions for this question. Returns the index of the
+	 * first occurrence of needle in haystack, or -1 if needle is not part of
+	 * haystack.
+	 *****/
 	public static int strStr(final String A, final String B) {
 
 		int a = A.length();
