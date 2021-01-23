90f8aa29dd0fce8f367014f4bf36a5bd680fb575
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2021-01-23 13:47:29 +0530

    Update ReorderList.java

--- src/LinkedList/ReorderList.java
+++ src/LinkedList/ReorderList.java
@@ -1,7 +1,17 @@
 package LinkedList;
 
 public class ReorderList {
-
+//https://leetcode.com/problems/reorder-list/
+		/******
+	 * Given a singly linked list L: L0→L1→…→Ln-1→Ln, reorder it to:
+	 * L0→Ln→L1→Ln-1→L2→Ln-2→…
+	 * 
+	 * You may not modify the values in the list's nodes, only nodes itself may be
+	 * changed.
+	 * 
+	 * ex. * Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+	 * 
+	 ******/
 	public static void main(String[] args) {
 
 	}
