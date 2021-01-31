533cef247ed7f146e0a12c9db38ef6c443e57026
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2021-01-31 22:56:51 +0530

    Update NextPointerBinaryTree.java

--- src/Trees/NextPointerBinaryTree.java
+++ src/Trees/NextPointerBinaryTree.java
@@ -8,6 +8,9 @@ public class NextPointerBinaryTree {
 	public static void main(String[] args) {
 
 	}
+	// Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+// Initially, all next pointers are set to NULL.
 
 	public void connect(TreeLinkNode root) {
 
