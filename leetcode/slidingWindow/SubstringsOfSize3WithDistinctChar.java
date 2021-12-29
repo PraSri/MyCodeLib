e0e0c38e6ee749a2dff327ecc718a53cfb493673
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2021-12-29 15:35:32 +0530

    Create SubstringsOfSize3WithDistinctChar.java

---
+++ leetcode/slidingWindow/SubstringsOfSize3WithDistinctChar.java
@@ -0,0 +1,34 @@
+package leetcode.slidingWindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubstringsOfSize3WithDistinctChar {
+  
+  
+  //  Substrings of Size Three with Distinct Characters
+
+    public static void main(String[] args) {
+        System.out.println(countGoodSubstrings("aababcabc"));
+    }
+    public static int countGoodSubstrings(String s) {
+        int n = s.length();
+        int ans = 0;
+        for(int i = 0;i<=n-3;i++) {
+            String sub = s.substring(i, i+3);
+            // System.out.println(" sub = " + sub);
+            int c = 0 ;
+            Set<Character> set = new HashSet();
+            for(char ch : sub.toCharArray()) {
+                if(set.add(ch)) {
+                    c++;
+                }
+                // System.out.println(set + " - " + c);
+            }
+            if(c==3) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
