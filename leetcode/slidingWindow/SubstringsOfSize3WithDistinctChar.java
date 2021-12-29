cda5134ae06fc6405e48eb2b80d553682aacd3ba
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2021-12-29 15:37:59 +0530

    Update SubstringsOfSize3WithDistinctChar.java

--- leetcode/slidingWindow/SubstringsOfSize3WithDistinctChar.java
+++ leetcode/slidingWindow/SubstringsOfSize3WithDistinctChar.java
@@ -31,4 +31,11 @@ public class SubstringsOfSize3WithDistinctChar {
         }
         return ans;
     }
+  
+  // solution without set
+  int countGoodSubstrings(string s,int r=0) {
+        for(int i=1;i<s.size()-1;i++)
+            if(s[i]!=s[i-1] && s[i]!=s[i+1] && s[i-1]!=s[i+1])r++;   
+        return r;
+    }
 }
