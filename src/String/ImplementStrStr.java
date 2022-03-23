ccad57fc3ee8d829d61d43f8183b89fc34c28ce5
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2022-03-24 01:06:57 +0530

    ImplementStrStr-leetcode-accepted-solution

--- src/String/ImplementStrStr.java
+++ src/String/ImplementStrStr.java
@@ -39,5 +39,75 @@ public class ImplementStrStr {
 		return -1;
 
 	}
+	
+	// correct accepted by leetcode
+	
+    public int strStr(String haystack, String needle) {
+        
+        if(true) {
+            return getIndexOf(haystack, needle);
+        }
+        
+        int big = haystack.length();
+        
+        int small = needle.length();
+       
+        if(small == 0){
+            return 0;
+        }
+        
+        if(big == 0){
+            return -1;
+        }
+        
+        for(int i = 0; i<big; i++) {
+            
+            if(i+small > big) {
+                break;
+            }
+            
+            for(int j = 0; j<small; j++) {
+                
+                if(haystack.charAt(i+j)!=needle.charAt(j)) {
+                    break;
+                }
+                
+                if(j == small-1) {
+                    return i;
+                }
+                
+            }
+            
+            
+        }
+        
+        return -1;
+        
+    }
+    
+    public int getIndexOf(String h, String n) {
+        
+        
+        int hl = h.length();
+        int nl = n.length();
+        
+        if(nl>hl){
+            return -1;
+        }
+        
+        if(nl==0){
+            return 0;
+        }
+        
+        int diff = hl-nl;
+        
+        for(int i =0 ;i<=diff;i++){
+            if(h.substring(i,i+nl).equals(n)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
 
 }
