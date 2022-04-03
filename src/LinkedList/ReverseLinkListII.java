bba021dc2ec783a03916f357c3484ea2ffbef712
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2022-04-04 01:16:04 +0530

    Update ReverseLinkListII.java

--- src/LinkedList/ReverseLinkListII.java
+++ src/LinkedList/ReverseLinkListII.java
@@ -62,4 +62,12 @@ public class ReverseLinkListII {
 		node.next = prev;
 		return fakeHead.next;
 	}
+	
+// 	for 1->2->3->4->5->null, m = 2, n = 4,
+// after the second while, we got 1<->2<-3<-4 5->null
+// now cur = 5, pre = 4, node = 1,
+// so node.next = 2, node.next.next = cur means 2->5;
+// node.next = pre means 1->4, then we get the result 1->4->3->2->5->null.
+	
+	
 }
