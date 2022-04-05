96b6f552dff68f00bb891c809cbcd3a91884ad28
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2022-04-05 23:18:55 +0530

    Create SubarrayProductLessThanK.java

---
+++ src/slidingWindow/SubarrayProductLessThanK.java
@@ -0,0 +1,27 @@
+
+// https://leetcode.com/problems/subarray-product-less-than-k/
+
+public int numSubarrayProductLessThanK(int[] nums, int k) {
+        
+        if(k<=1)
+            return 0;
+        
+        int prod = 1, ans = 0, left = 0, right = 0, n = nums.length;
+        
+        while(right < n) {
+            
+            prod = prod * nums[right]; 
+            while(prod >= k) {
+                prod = prod / nums[left]; 
+                left++; // window contacting
+            }
+            
+            ans = ans + (right-left+1); 
+            
+            right++; // winow expanding
+            
+        }
+        
+        return ans;
+        
+    }
