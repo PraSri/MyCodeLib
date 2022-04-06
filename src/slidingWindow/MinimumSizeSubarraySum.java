dd65688fd7b9658f1c3ced15667bbbe034d3ba5b
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2022-04-06 17:25:31 +0530

    Create MinimumSizeSubarraySum.java

---
+++ src/slidingWindow/MinimumSizeSubarraySum.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+
+public int minSubArrayLen(int target, int[] nums) {
+        
+        int n = nums.length;
+        int sum = 0, s = 0, e = 0, min = Integer.MAX_VALUE;
+        
+        while(e<n) {
+            
+            sum += nums[e++]; // expanding
+            
+            while(sum>=target) {
+                
+                min = Math.min(min, e-s);
+                
+                sum -= nums[s++]; // contracting
+                
+            }
+    
+        }
+        
+        return min == Integer.MAX_VALUE ? 0 : min;
+        
+    }
