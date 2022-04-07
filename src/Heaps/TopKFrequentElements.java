d993b12705ba10a8d0013eed95b45d6926611f52
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2022-04-07 23:25:58 +0530

    Create TopKFrequentElements.java

---
+++ src/Heaps/TopKFrequentElements.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+public int[] topKFrequent(int[] nums, int k) {
+        
+        Map<Integer, Integer> frequency = new HashMap<>();
+        
+        for(int x : nums) {
+            frequency.put(x, frequency.getOrDefault(x, 0) + 1);
+        }
+        
+        Queue<Integer> minHeap = new PriorityQueue<>(
+            (x1, x2) -> frequency.get(x1) - frequency.get(x2)
+        );
+        
+        for(int x : frequency.keySet()) {
+            minHeap.add(x);
+            if(minHeap.size()>k) {
+                minHeap.poll();
+            }
+        }
+        
+        int[] top = new int[k];
+        
+        for(int i = k-1; i>=0; i--) {
+            top[i] = minHeap.poll();
+        }
+        
+        return top;
+    }
