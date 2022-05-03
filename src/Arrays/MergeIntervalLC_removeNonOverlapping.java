176726b59b1b420e4b2d1db74d6e79af44880181
PRAKHAR SRIVASTAVA <prakharsri94@yahoo.com>
2022-05-03 18:57:19 +0530

    Create MergeIntervalLC_removeNonOverlapping.java

---
+++ src/Arrays/MergeIntervalLC_removeNonOverlapping.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+public class MergeIntervalLC_removeNonOverlapping {
+  public class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Interval[] arr = new Interval[intervals.length];
+        int i = 0;
+        for (int[] interval : intervals) {
+            arr[i++] = new Interval(interval[0], interval[1]);
+        }
+        return solve(arr);
+    }
+
+    public int solve(Interval[] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a.end));
+        int c = 1;
+        int end = intervals[0].end;
+        for(int i = 1; i<intervals.length; i++) {
+            if(intervals[i].start>=end) {
+                end = intervals[i].end;
+                c++;
+            }
+        }
+        return intervals.length - c;
+    }
+}
