f1648b296ad402f92833e7108612078b39a4e0b0
Prakhar Srivastava <prasri510@gmail.com>
2022-08-17 15:13:28 +0530

    LongestRepeatingCharacterReplacement

---
+++ leetcode/leetcode75/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,31 @@
+package leetcode75;
+
+public class LongestRepeatingCharacterReplacement {
+
+    /**
+     *
+     * https://leetcode.com/problems/longest-repeating-character-replacement/
+    * */
+
+    public int characterReplacement(String st, int k) {
+        int[] f = new int[26];
+        int mostFreqLetter = 0;
+        int s = 0;
+        int e = 0;
+        int n = st.length();
+        int max = 0;
+
+        while(e < n) {
+            f[st.charAt(e)-'A']++;
+            mostFreqLetter = Math.max(mostFreqLetter, f[st.charAt(e)-'A']);
+            int changes = (e-s+1)-mostFreqLetter;
+            if(changes > k) {
+                f[st.charAt(s) - 'A']--;
+                s++;
+            }
+            max = Math.max(max, (e-s+1));
+            e++;
+        }
+        return max;
+    }
+}
