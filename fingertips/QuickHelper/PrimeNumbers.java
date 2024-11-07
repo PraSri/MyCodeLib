cc3e0b2e1f72d42161470a9ac8331658d67ee6ac
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2024-11-07 23:06:50 +0530

    Update PrimeNumbers.java

--- fingertips/QuickHelper/PrimeNumbers.java
+++ fingertips/QuickHelper/PrimeNumbers.java
@@ -46,4 +46,36 @@ public class PrimeNumbers {
 		return primes;
 	}
 
+	// check prime number 
+
+public static void checkPrime() {
+    // Input number
+    int n = 11;
+
+    // Holds the count of values
+    int count = 0;
+
+    // There is no prime number less than 2.
+    if (n <= 1) {
+      System.out.println("The number is not prime");
+      return;
+    }
+
+    // Do a for loop
+    for (int j = 1; j <= n / 2; j++) {
+      if (n % j == 0) {
+        count++;
+      }
+    }
+
+    // If the number of factors is greater than 1,
+    // the number is not prime.
+    if (count > 1) {
+        System.out.println("The number is not prime");
+    }
+    else {
+      System.out.println("The number is prime");
+    }
+  }
+
 }
