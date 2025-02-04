c3c631e41a9d21a7176ee33bf14071502ae3da20
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2025-02-04 23:58:22 +0530

    Create EduTreeNode.java

---
+++ educative2025/two_pointers/EduTreeNode.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class EduTreeNode {
+    int data;
+    EduTreeNode left;
+    EduTreeNode right;
+    EduTreeNode parent;
+
+    EduTreeNode(int value) {
+        this.data = value;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+    }
+}
