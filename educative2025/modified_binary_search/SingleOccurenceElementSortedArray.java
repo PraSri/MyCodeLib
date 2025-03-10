ec9ae33102be75d38fb2a31ad4232130a7bda78f
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2025-03-10 23:25:02 +0530

    Create SingleOccurenceElementSortedArray.java

---
+++ educative2025/modified_binary_search/SingleOccurenceElementSortedArray.java
@@ -0,0 +1,41 @@
+class SingleOccurenceElementSortedArray {
+    public static int singleNonDuplicate(int[] nums) {
+
+        // initilaize the left and right pointer
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+           
+            // if mid is odd, decrement it to make it even
+            int mid = l + (r - l) / 2;
+            if (mid % 2 == 1) mid--;
+            
+            // if the elements at mid and mid + 1 are the same, then the single element must appear after the midpoint
+            if (nums[mid] == nums[mid + 1]) {
+                l = mid + 2;
+            } 
+            // otherwise, we must search for the single element before the midpoint
+            else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        int[][] inputs = {
+            {1, 2, 2, 3, 3, 4, 4},
+            {1, 1, 2, 2, 3, 4, 4, 5, 5},
+            {1, 1, 2, 3, 3},
+            {1, 1, 2},
+            {0, 2, 2, 3, 3, 4, 4, 5, 5}
+        };
+        for (int i = 0; i < inputs.length; i++) {
+            System.out.print(i + 1);
+            System.out.println(".\tInput array: " + Arrays.toString(inputs[i]));
+            System.out.println("\n\tSingle element found: " + singleNonDuplicate(inputs[i]));
+            System.out.println(PrintHyphens.repeat("-", 100));        }
+    }
+}
