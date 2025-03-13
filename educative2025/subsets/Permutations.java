fe0d275f966ee6c544904351b8580b2da7ece77a
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2025-03-13 22:36:13 +0530

    Create Permutations.java

---
+++ educative2025/subsets/Permutations.java
@@ -0,0 +1,43 @@
+class Permutations {
+ 
+    public static String swapChar(String word,int i, int j) 
+    {
+        char[] swapIndex = word.toCharArray();
+        char temp = swapIndex[i];
+        swapIndex[i] = swapIndex[j];
+        swapIndex[j] = temp;
+
+        return new String(swapIndex);
+    }
+
+    public static  void permuteStringRec(String word, int currentIndex, ArrayList<String> results)
+    {
+        if(currentIndex == word.length() - 1)
+        {
+            results.add(word);
+            return ;
+        }
+        for (int index = currentIndex; index < word.length(); index++)
+         {
+            String swappedStr = swapChar(word, currentIndex, index);
+            permuteStringRec(swappedStr, currentIndex + 1, results);
+         }
+    }
+    public static ArrayList<String> permuteWord(String word)
+    {
+        ArrayList<String> results = new ArrayList<String>();
+        permuteStringRec(word, 0, results);
+        return results;
+    }
+    // Driver code
+    public static void main( String args[] ) {
+        String[] inputWord = {"ab", "bad", "abcd"};
+        for (int index = 0; index < inputWord.length; index++)
+        {
+            ArrayList <String> permutedWords = permuteWord(inputWord[index]);
+            System.out.println(index + 1 + ".\t Input string: '" + inputWord[index] + "'");
+            System.out.println("\t All possible permutations are: " + permutedWords);
+             System.out.println(PrintHyphens.repeat("-", 100));
+        }
+    }
+}
