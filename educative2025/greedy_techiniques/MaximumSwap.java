562c5b1960d02e379c95a53db7b46b55b871f17d
PRAKHAR SRIVASTAVA <prasri510@gmail.com>
2025-03-19 00:45:47 +0530

    Create MaximumSwap.java

---
+++ educative2025/greedy_techiniques/MaximumSwap.java
@@ -0,0 +1,46 @@
+public class MaximumSwap {
+    public static int maximumSwap(int num) {
+        // Convert the number to a string for easy manipulation
+        char[] numString = String.valueOf(num).toCharArray();
+        int n = numString.length;
+
+        // Variables to track the index of the maximum digit and the two swap indices
+        int maxDigitIndex = -1, index1 = -1, index2 = -1;
+
+        // Iterate the string from the last digit to the first
+        for (int i = n - 1; i >= 0; i--) {
+            // Update the max digit index if the current digit is greater
+            if (maxDigitIndex == -1 || numString[i] > numString[maxDigitIndex]) {
+                maxDigitIndex = i;
+            }
+            // If the current digit is less than the max digit found so far,
+            // mark this index (index1) and the max digit's index (index2) for swapping
+            else if (numString[i] < numString[maxDigitIndex]) {
+                index1 = i;
+                index2 = maxDigitIndex;
+            }
+        }
+
+        // Perform the swap if valid indices are found
+        if (index1 != -1 && index2 != -1) {
+            char temp = numString[index1];
+            numString[index1] = numString[index2];
+            numString[index2] = temp;
+        }
+
+        // Convert the string back to an integer and return it
+        return Integer.parseInt(new String(numString));
+    }
+    
+    // Driver code
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4121, 87654, 1643, 123, 14};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println((i + 1) + ".\tNumber: " + nums[i]);
+            System.out.println("\n\tLargest number after swapping: " + solution.maximumSwap(nums[i]));
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+        }
+    }
+}
