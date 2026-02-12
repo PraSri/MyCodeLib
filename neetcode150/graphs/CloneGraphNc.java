6b3956b1583dc2867baad1855ca221b5c00554d0
prakharsrivastava <prasri510@gmail.com>
2026-02-13 01:31:30 +0530

    Add implementations for cloning linked lists and trees with random pointers

--- neetcode150/graphs/CloneGraphNc.java
+++ neetcode150/graphs/CloneGraphNc.java
@@ -1,6 +1,8 @@
 package graphs;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -44,7 +46,60 @@ public class CloneGraphNc {
      * Copy List with Random Pointer
      * https://leetcode.com/problems/copy-list-with-random-pointer
      */
+
     public static class CopyListWithRandomPointer {
+        // space O(1)
+        // 1. iterate the original list and create duplicate node next to original node
+        // 2. iterate new list and assign random pointer for each duplicated node
+        // 3. restore the original list and extract the duplicate list
+
+        public Node copyRandomList(Node head) {
+            Node curr = head, next;
+            while (curr != null) {
+                next = curr.next;
+                Node copy = new Node(curr.val);
+                curr.next = copy;
+                copy.next = next;
+                curr = next;
+            }
+            curr = head;
+            while (curr != null) {
+                if (curr.random != null) {
+                    curr.next.random = curr.random.next;
+                }
+                curr = curr.next.next;
+            }
+            curr = head;
+            Node dummyHead = new Node(-1);
+            Node copy, copyCurr = dummyHead;
+            while (curr != null) {
+                next = curr.next.next;
+
+                // extract copy
+                copy = curr.next;
+                copyCurr.next = copy;
+                copyCurr = copy;
+
+                // restore current
+                curr.next = next;
+
+                curr = next;
+            }
+            return dummyHead.next;
+        }
+
+        public static class Node {
+            int val;
+            Node next;
+            Node random;
+
+            public Node(int val) {
+                this.val = val;
+                this.next = null;
+                this.random = null;
+            }
+        }
+
     }
 
     /**
@@ -52,6 +107,56 @@ public class CloneGraphNc {
      * https://leetcode.com/problems/clone-binary-tree-with-random-pointer
      */
     public static class CloneBinaryTreeWithRandomPointer {
+
+        static Node cloneTree(Node root) {
+            if (root == null) {
+                return null;
+            }
+            // map to store mapping between original and cloned node
+            Map<Node, Node> map = new HashMap<>();
+            // copyLeftRightNode
+            Node newRoot = copyLeftRightNode(root, map);
+            // copyRandomPointer
+            copyRandom(root, map);
+            // return new tree
+            return newRoot;
+        }
+
+        private static void copyRandom(Node root, Map<Node, Node> map) {
+            if (root == null) {
+                return;
+            }
+
+            map.get(root).random = map.get(root.random);
+            //recur
+            copyRandom(root.left, map);
+            copyRandom(root.right, map);
+
+        }
+
+        private static Node copyLeftRightNode(Node root, Map<Node, Node> map) {
+            if (root == null) {
+                return null;
+            }
+            Node clonedNode = new Node(root.x);
+            map.put(root, clonedNode);
+            // recursive
+            clonedNode.left = copyLeftRightNode(root.left, map);
+            clonedNode.right = copyLeftRightNode(root.right, map);
+
+            return clonedNode;
+        }
+
+        static class Node {
+            Node left, right, random;
+            int x;
+
+            public Node(int x) {
+                this.x = x;
+                left = right = random = null;
+            }
+        }
+
     }
 
     /**
@@ -59,5 +164,27 @@ public class CloneGraphNc {
      * https://leetcode.com/problems/clone-n-ary-tree
      */
     public static class CloneNAryTree {
+        public Node cloneNAryTree(Node root) {
+            if (root == null) {
+                return null;
+            }
+            List<Node> clonedChildren = new ArrayList<>();
+            //recur
+            for (Node child : root.children) {
+                clonedChildren.add(cloneNAryTree(child));
+            }
+            return new Node(root.x, clonedChildren);
+        }
+
+        public static class Node {
+            int x;
+            List<Node> children;
+
+            public Node(int x, List<Node> children) {
+                this.x = x;
+                this.children = new ArrayList<>();
+                this.children.addAll(children);
+            }
+        }
     }
 }
--- neetcode150/linkedlist/CopyLinkedListWithRandomPointer.java
+++ neetcode150/linkedlist/CopyLinkedListWithRandomPointer.java
@@ -33,6 +33,63 @@ public class CopyLinkedListWithRandomPointer {
         return map.get(head);
     }
 
+    /**
+     * Space O(1)
+     * The algorithm is composed of the follow three steps which are also 3 iteration rounds.
+     * <p>
+     * Iterate the original list and duplicate each node. The duplicate
+     * of each node follows its original immediately.
+     * Iterate the new list and assign the random pointer for each
+     * duplicated node.
+     * Restore the original list and extract the duplicated nodes.
+     */
+
+    public Node copyRandomList2(Node head) {
+        Node iter = head, next;
+
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+        while (iter != null) {
+            next = iter.next;
+
+            Node copy = new Node(iter.val);
+            iter.next = copy;
+            copy.next = next;
+
+            iter = next;
+        }
+
+        // Second round: assign random pointers for the copy nodes.
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        // Third round: restore the original list, and extract the copy list.
+        iter = head;
+        Node pseudoHead = new Node(0);
+        Node copy, copyIter = pseudoHead;
+
+        while (iter != null) {
+            next = iter.next.next;
+
+            // extract the copy
+            copy = iter.next;
+            copyIter.next = copy;
+            copyIter = copy;
+
+            // restore the original list
+            iter.next = next;
+
+            iter = next;
+        }
+
+        return pseudoHead.next;
+    }
+
     public static class Node {
         int val;
         Node next;
@@ -44,6 +101,7 @@ public class CopyLinkedListWithRandomPointer {
             this.random = null;
         }
     }
+
     /**
      * <a href="https://leetcode.com/problems/clone-graph/">LeetCode - Clone Graph</a>
      */
