2f4a4f68bf10c06e146848bc4f03cac44013b738
prakharsrivastava <prasri510@gmail.com>
2026-03-19 00:17:20 +0530

    Archived old things

---
+++ archived/educative2025/greedy_techiniques/BestTimeBuySellStock.java
@@ -0,0 +1,17 @@
+package greedy_techiniques;
+
+class BestTimeBuySellStock {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int res = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            if (prices[i] - min > res) {
+                res = prices[i] - min;
+            }
+        }
+        return res;
+    }
+}
---
+++ archived/educative2025/greedy_techiniques/JumpGame.java
@@ -0,0 +1,18 @@
+package greedy_techiniques;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+
+        int n = nums.length;
+        int targetIndex = n - 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (targetIndex <= (i + nums[i])) {
+                targetIndex = i;
+            }
+        }
+
+        return targetIndex == 0;
+
+    }
+}
